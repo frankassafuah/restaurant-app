97dc73cffbd36f8a9540e86145a866994833ecdd
Frank Assafuah <frankassafuah@gmail.com>
2022-11-10 12:02:40 +0000

    IMP- Display user name

--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <Header/>
-        <h1>Hello user, welcome to Home Page</h1>
+        <h1>Hello {{user.name}}, welcome to Home Page</h1>
     </div>
 </template>
 
@@ -12,13 +12,20 @@ export default {
     components: {
         Header
     },
-
+    data(){
+        return {
+            user: {}
+        }
+    },
     created(){
         //redirect user to sign up page if user is not logged in
         let user = localStorage.getItem('user-info');
+        this.user = JSON.parse(user);
         if(!user){
             this.$router.push({name: 'SignUp'})
         }
+    },
+    methods: {
     }
 }
 
