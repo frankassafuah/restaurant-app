1b0c00815d43e0ab7f72274244683f5042cf594c
Frank Assafuah <frankassafuah@gmail.com>
2022-11-10 11:09:33 +0000

    IMP- Add and Update restaurant components

---
+++ src/components/update.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Header/>
+        <h1>Hello user, welcome to Update Restaurant Page</h1>
+    </div>
+</template>
+
+<script>
+import Header from '../components/header.vue'
+export default {
+    name: 'Update',
+    components: {
+        Header
+    },
+
+    created(){
+        //redirect user to sign up page if user is not logged in
+        let user = localStorage.getItem('user-info');
+        if(!user){
+            this.$router.push({name: 'SignUp'})
+        }
+    }
+}
+
+</script>
