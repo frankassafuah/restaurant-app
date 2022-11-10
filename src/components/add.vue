7c4bf06205b0506d251d5a31e5c3cfe2ab42f34c
Frank Assafuah <frankassafuah@gmail.com>
2022-11-10 16:54:17 +0000

    IMP- Add restaurant table and bindings

--- src/components/add.vue
+++ src/components/add.vue
@@ -2,6 +2,12 @@
     <div>
         <Header/>
         <h1>Hello user, welcome to Add Restaurant Page</h1>
+        <form class="addForm">
+            <input type="text" placeholder="Enter Name" v-model="form.name">
+            <input type="text" placeholder="Enter Address" v-model="form.address">
+            <input type="text" placeholder="Enter Contact" v-model="form.contact">
+            <button type="button">Add new restaurant</button>
+        </form>
     </div>
 </template>
 
@@ -12,7 +18,11 @@ export default {
     components: {
         Header
     },
-
+    data(){
+        return {
+            form: {}
+        }
+    },
     created(){
         //redirect user to sign up page if user is not logged in
         let user = localStorage.getItem('user-info');
@@ -23,3 +33,25 @@ export default {
 }
 
 </script>
+
+<style scoped> 
+.addForm { 
+    width:50% ;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    flex-direction: column;
+    }
+
+    .addForm input {
+        width: 50%;
+        height: 35px;
+        margin: 0px auto 10px auto;
+    }
+
+    .addForm button {
+        width: 20%;
+        margin: 0 auto ;
+        height: 40px;
+    }
+</style>
--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,32 +1,69 @@
 <template>
-    <div>
-        <Header/>
-        <h1>Hello {{user.name}}, welcome to Home Page</h1>
-    </div>
+  <div>
+    <Header />
+    <h1>Hello {{ user.name }}, welcome to Home Page</h1>
+    <table border="1px">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Contact</th>
+        <th>Address</th>
+      </tr>
+      <tr v-for="restaurant in restaurants" :key="restaurant.id">
+        <td>{{ restaurant.id }}</td>
+        <td>{{ restaurant.name }}</td>
+        <td>{{ restaurant.contact }}</td>
+        <td>{{ restaurant.address }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-import Header from '../components/header.vue'
+import Header from "../components/header.vue";
+import axios from "axios";
 export default {
-    name: 'HomePage',
-    components: {
-        Header
-    },
-    data(){
-        return {
-            user: {}
-        }
-    },
-    created(){
-        //redirect user to sign up page if user is not logged in
-        let user = localStorage.getItem('user-info');
-        this.user = JSON.parse(user);
-        if(!user){
-            this.$router.push({name: 'SignUp'})
-        }
-    },
-    methods: {
+  name: "HomePage",
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      user: {},
+      restaurants: [],
+    };
+  },
+  created() {
+    //redirect user to sign up page if user is not logged in
+    let user = localStorage.getItem("user-info");
+    this.user = JSON.parse(user);
+    if (!user) {
+      this.$router.push({ name: "SignUp" });
     }
+
+    this.getRestaurants();
+  },
+  methods: {
+    async getRestaurants() {
+      try {
+        const response = await axios.get("http://localhost:3000/restaurants");
+        if (response) {
+          this.restaurants = response.data;
+        }
+      } catch (error) {}
+    },
+  },
+};
+</script>
+
+<style scoped>
+table{
+    margin: 0 auto ;
 }
 
-</script>
+th, td {
+    width: 160px;
+    height: 40px;
+}
+
+</style>
