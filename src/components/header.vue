aeaaf411140becf73f614ddfa1aeb9b595855cb5
Frank Assafuah <frankassafuah@gmail.com>
2022-11-10 09:00:37 +0000

    IMP- Header nav and styles

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <h1>Header</h1>
+    <div class="nav">
+        <a href="#">Home</a>
+        <a href="#">Add Restaurant</a>
+        <a href="#">Update Restaurant</a>
+        <a href="#">Logout</a>
     </div>
 </template>
 
@@ -12,3 +15,25 @@ export default {
 }
 
 </script>
+
+<style scoped>
+.nav {
+    background-color: #333;
+    overflow: hidden;
+}
+
+.nav a {
+    color: #f2f2f2f2;
+    float: left;
+    text-align: center;
+    text-decoration: none;
+    padding: 14px 16px;
+    margin-right: 5px;
+    font-size: 17px;
+}
+
+.nav a:hover {
+    background-color: #ddd;
+    color: #333
+}
+</style>
